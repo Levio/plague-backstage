68aa9b7b3310733d00d771ce5a4b70f8b5f00bd6
jianbin.liao <jianbin.liao@aseit.cn>
2020-03-05 16:09:07 +0800

    init project

---
+++ config/config.ts
@@ -0,0 +1,47 @@
+import { IConfig } from 'umi-types';
+import routes from './router.config';
+import proxy from './proxy.config';
+
+// ref: https://umijs.org/config/
+const config: IConfig = {
+  treeShaking: true,
+  routes,
+  hash: true,
+  targets: {
+    ie: 11,
+  },
+  ignoreMomentLocale: true,
+  lessLoaderOptions: {
+    javascriptEnabled: true,
+  },
+  disableRedirectHoist: true,
+  manifest: {
+    basePath: '/',
+  },
+  proxy,
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    [
+      'umi-plugin-react',
+      {
+        antd: true,
+        dva: true,
+        dynamicImport: false,
+        title: '疫镜后台管理平台',
+        dll: false,
+
+        routes: {
+          exclude: [
+            /models\//,
+            /services\//,
+            /model\.(t|j)sx?$/,
+            /service\.(t|j)sx?$/,
+            /components\//,
+          ],
+        },
+      },
+    ],
+  ],
+};
+
+export default config;
